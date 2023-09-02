1ed8ae1e9aa680112736d9c43946cd14cf47e392
antoniocristovam <antoniocristovam@outlook.com.br>
2023-09-02 20:52:23 -0300

    feat: add routes delete

--- src/app/controllers/contactController.js
+++ src/app/controllers/contactController.js
@@ -1,37 +1,64 @@
-const ContactRepositoty = require("../repositories/ContactRepositoty");
 const ContactsRepository = require("../repositories/ContactRepositoty");
+
+// Centralizar toda regras de negocios.
 
 class ContactController {
   async index(request, response) {
-    // Listar todos os projetos
-
     const contacts = await ContactsRepository.findAll();
     response.json(contacts);
   }
 
   async show(request, response) {
-    // Obter um Registro
-    // response.json(request.params);
     const { id } = request.params;
-    const contact = await ContactRepositoty.findById(id);
+    const contact = await ContactsRepository.findById(id);
 
     if (!contact) {
-      return response.status(404).json({ error: "User not found" });
-    } else {
-      response.json(contact);
+      return response.status(404).json({ error: "User not found." });
     }
+    response.json(contact);
   }
 
-  store() {
-    // Criar um novo registro
+  async store(request, response) {
+    const { name, email, phone, category_id } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: "Name is required." });
+    }
+
+    const contactExists = await ContactsRepository.findByEmail(email);
+
+    if (contactExists) {
+      return response
+        .status(400)
+        .json({ error: "This e-mail is already been taken." });
+    }
+    const contacts = await ContactsRepository.create({
+      name,
+      email,
+      phone,
+      category_id,
+    });
+
+    response.json(contacts);
   }
 
   update() {
     // Editar um registro
   }
 
-  delete() {
+  async delete(request, response) {
     // Deletar um registro
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: "User not found." });
+    }
+    await ContactsRepository.delete(id);
+
+    //204: Not Content
+    response.json({ Success: "Contato deletado com sucesso." });
   }
 }
 
--- src/app/repositories/ContactRepositoty.js
+++ src/app/repositories/ContactRepositoty.js
@@ -1,6 +1,6 @@
 const { v4 } = require("uuid");
 
-const contacts = [
+let contacts = [
   {
     id: v4(),
     name: "Antonio Cristovam",
@@ -11,6 +11,27 @@ const contacts = [
   {
     id: v4(),
     name: "Valderi Cristovam",
+    email: "deircristovam@outlook.com.br",
+    phone: "973027579",
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: "Armando Cristovam",
+    email: "deircristovam@outlook.com.br",
+    phone: "973027579",
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: "Julia arnaldo",
+    email: "deircristovam@outlook.com.br",
+    phone: "973027579",
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: "Paulo vieira",
     email: "deircristovam@outlook.com.br",
     phone: "973027579",
     category_id: v4(),
@@ -27,6 +48,33 @@ class ContactRepository {
   findById(id) {
     return new Promise((resolve) => {
       resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+
+  create({ name, email, phone, category_id }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: v4(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts.push(newContact);
+      resolve(newContact);
     });
   }
 }
